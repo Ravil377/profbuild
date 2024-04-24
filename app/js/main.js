3c0ce1209894fbd33f31b66682df9dcbea34f7bf
Ravil <75186385+Ravil377@users.noreply.github.com>
2024-04-24 14:08:03 -0800

    first push

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@
   \*******************************/
 /***/ (() => {
 
-console.log('components');
+
 
 /***/ }),
 
@@ -179,49 +179,51 @@ __webpack_require__.r(__webpack_exports__);
 
 
 (function () {
-  const burger = document?.querySelector('[data-burger]');
-  const menu = document?.querySelector('[data-menu]');
-  const logo = document?.querySelector('[data-logo]');
-  const header = document?.querySelector('[data-header]');
-  const menuItems = document?.querySelectorAll('[data-menu-item]');
-  const overlay = document?.querySelector('[data-menu-overlay]');
-  const topMenuItem = document?.querySelector('[data-menu-item="top"]');
-  burger?.addEventListener('click', e => {
-    burger?.classList.toggle('burger--active');
-    menu?.classList.toggle('menu--active');
-    logo?.classList.toggle('logo--active');
-    header?.classList.toggle('header--active');
-    if (menu?.classList.contains('menu--active')) {
-      burger?.setAttribute('aria-expanded', 'true');
-      burger?.setAttribute('aria-label', 'Закрыть меню');
-      (0,_functions_disable_scroll__WEBPACK_IMPORTED_MODULE_0__.disableScroll)();
-    } else {
-      burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Открыть меню');
-      (0,_functions_enable_scroll__WEBPACK_IMPORTED_MODULE_1__.enableScroll)();
-    }
-    if (topMenuItem) {
-      topMenuItem.scrollIntoView({
-        behavior: "smooth"
-      });
-    }
-  });
-  overlay?.addEventListener('click', () => {
-    burger?.setAttribute('aria-expanded', 'false');
-    burger?.setAttribute('aria-label', 'Открыть меню');
-    burger.classList.remove('burger--active');
-    menu.classList.remove('menu--active');
-    (0,_functions_enable_scroll__WEBPACK_IMPORTED_MODULE_1__.enableScroll)();
-  });
-  menuItems?.forEach(el => {
-    el.addEventListener('click', () => {
-      burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Открыть меню');
-      burger.classList.remove('burger--active');
-      menu.classList.remove('menu--active');
-      (0,_functions_enable_scroll__WEBPACK_IMPORTED_MODULE_1__.enableScroll)();
-    });
-  });
+  // const burger = document?.querySelector('[data-burger]');
+  // const menu = document?.querySelector('[data-menu]');
+  // const logo = document?.querySelector('[data-logo]');
+  // const header = document?.querySelector('[data-header]');
+  // const menuItems = document?.querySelectorAll('[data-menu-item]');
+  // const overlay = document?.querySelector('[data-menu-overlay]');
+  // const topMenuItem = document?.querySelector('[data-menu-item="top"]');
+
+  // burger?.addEventListener('click', (e) => {
+  //   burger?.classList.toggle('burger--active');
+  //   menu?.classList.toggle('menu--active');
+  //   logo?.classList.toggle('logo--active');
+  //   header?.classList.toggle('header--active');
+
+  //   if (menu?.classList.contains('menu--active')) {
+  //     burger?.setAttribute('aria-expanded', 'true');
+  //     burger?.setAttribute('aria-label', 'Закрыть меню');
+  //     disableScroll();
+  //   } else {
+  //     burger?.setAttribute('aria-expanded', 'false');
+  //     burger?.setAttribute('aria-label', 'Открыть меню');
+  //     enableScroll();
+  //   }
+  //   if (topMenuItem) {
+  //     topMenuItem.scrollIntoView({ behavior: "smooth" });
+  //   }
+  // });
+
+  // overlay?.addEventListener('click', () => {
+  //   burger?.setAttribute('aria-expanded', 'false');
+  //   burger?.setAttribute('aria-label', 'Открыть меню');
+  //   burger.classList.remove('burger--active');
+  //   menu.classList.remove('menu--active');
+  //   enableScroll();
+  // });
+
+  // menuItems?.forEach(el => {
+  //   el.addEventListener('click', () => {
+  //     burger?.setAttribute('aria-expanded', 'false');
+  //     burger?.setAttribute('aria-label', 'Открыть меню');
+  //     burger.classList.remove('burger--active');
+  //     menu.classList.remove('menu--active');
+  //     enableScroll();
+  //   });
+  // });
 })();
 
 /***/ }),
--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -2,49 +2,49 @@ import { disableScroll } from '../functions/disable-scroll';
 import { enableScroll } from '../functions/enable-scroll';
 
 (function(){
-  const burger = document?.querySelector('[data-burger]');
-  const menu = document?.querySelector('[data-menu]');
-  const logo = document?.querySelector('[data-logo]');
-  const header = document?.querySelector('[data-header]');
-  const menuItems = document?.querySelectorAll('[data-menu-item]');
-  const overlay = document?.querySelector('[data-menu-overlay]');
-  const topMenuItem = document?.querySelector('[data-menu-item="top"]');
+  // const burger = document?.querySelector('[data-burger]');
+  // const menu = document?.querySelector('[data-menu]');
+  // const logo = document?.querySelector('[data-logo]');
+  // const header = document?.querySelector('[data-header]');
+  // const menuItems = document?.querySelectorAll('[data-menu-item]');
+  // const overlay = document?.querySelector('[data-menu-overlay]');
+  // const topMenuItem = document?.querySelector('[data-menu-item="top"]');
 
-  burger?.addEventListener('click', (e) => {
-    burger?.classList.toggle('burger--active');
-    menu?.classList.toggle('menu--active');
-    logo?.classList.toggle('logo--active');
-    header?.classList.toggle('header--active');
+  // burger?.addEventListener('click', (e) => {
+  //   burger?.classList.toggle('burger--active');
+  //   menu?.classList.toggle('menu--active');
+  //   logo?.classList.toggle('logo--active');
+  //   header?.classList.toggle('header--active');
 
-    if (menu?.classList.contains('menu--active')) {
-      burger?.setAttribute('aria-expanded', 'true');
-      burger?.setAttribute('aria-label', 'Закрыть меню');
-      disableScroll();
-    } else {
-      burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Открыть меню');
-      enableScroll();
-    }
-    if (topMenuItem) {
-      topMenuItem.scrollIntoView({ behavior: "smooth" });
-    }
-  });
+  //   if (menu?.classList.contains('menu--active')) {
+  //     burger?.setAttribute('aria-expanded', 'true');
+  //     burger?.setAttribute('aria-label', 'Закрыть меню');
+  //     disableScroll();
+  //   } else {
+  //     burger?.setAttribute('aria-expanded', 'false');
+  //     burger?.setAttribute('aria-label', 'Открыть меню');
+  //     enableScroll();
+  //   }
+  //   if (topMenuItem) {
+  //     topMenuItem.scrollIntoView({ behavior: "smooth" });
+  //   }
+  // });
 
-  overlay?.addEventListener('click', () => {
-    burger?.setAttribute('aria-expanded', 'false');
-    burger?.setAttribute('aria-label', 'Открыть меню');
-    burger.classList.remove('burger--active');
-    menu.classList.remove('menu--active');
-    enableScroll();
-  });
+  // overlay?.addEventListener('click', () => {
+  //   burger?.setAttribute('aria-expanded', 'false');
+  //   burger?.setAttribute('aria-label', 'Открыть меню');
+  //   burger.classList.remove('burger--active');
+  //   menu.classList.remove('menu--active');
+  //   enableScroll();
+  // });
 
-  menuItems?.forEach(el => {
-    el.addEventListener('click', () => {
-      burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Открыть меню');
-      burger.classList.remove('burger--active');
-      menu.classList.remove('menu--active');
-      enableScroll();
-    });
-  });
+  // menuItems?.forEach(el => {
+  //   el.addEventListener('click', () => {
+  //     burger?.setAttribute('aria-expanded', 'false');
+  //     burger?.setAttribute('aria-label', 'Открыть меню');
+  //     burger.classList.remove('burger--active');
+  //     menu.classList.remove('menu--active');
+  //     enableScroll();
+  //   });
+  // });
 })();
